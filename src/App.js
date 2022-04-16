a78690b8f4cf33f81c9f70b69ce66208996de667
Ben Caterine <benrcaterine@gmail.com>
2022-04-16 14:16:01 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import * as V from 'victory';
+import { VictoryLine } from 'victory';
+import { VictoryChart } from 'victory';
+import { VictoryTheme } from 'victory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VictoryChart
+        theme={VictoryTheme.material}
+      >
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 }
+          ]}
+        />
+      </VictoryChart>
+      <pre>
+        
+      </pre>
     </div>
   );
 }
