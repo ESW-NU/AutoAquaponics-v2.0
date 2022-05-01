0b4564bb70141e29512b602ac867d6af1e72d7de
Ben Caterine <benrcaterine@gmail.com>
2022-05-01 14:07:21 -0500

    live graphs with fake data

--- src/App.js
+++ src/App.js
@@ -31,6 +31,19 @@ const DashboardGraphs = ({graphData}) => {
 
 const LiveGraph = ({title}) => {
 
+  const [data, setData] = useState([{a:0, b:1, c:2}, {a:1, b:2, c:3}, {a:2, b:4, c:5}, {a:3, b:3, c:7}, {a:4, b:5, c:6}, {a:5, b:7, c:8}]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setData((prevData) => {
+        const d = prevData.slice(-1)[0]
+        return [...prevData, {a:d.a+1, b:d.b+1, c:d.c+1}];
+      })
+    }, 2000);
+  
+    return () => clearInterval(interval);
+  }, []);
+
   return(
     <div>
       <VictoryLabel text={title} x={225} y={30} textAnchor="middle"/>
@@ -44,13 +57,16 @@ const LiveGraph = ({title}) => {
             parent: { border: "1px solid #ccc"}
           }}
           
-          data={[
-            { x: 1, y: 2 },
-            { x: 2, y: 3 },
-            { x: 3, y: 5 },
-            { x: 4, y: 4 },
-            { x: 5, y: 7 }
-          ]}
+          data={data.map(d => ({x:d.a, y:d.b}))}
+
+        />
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          
+          data={data.map(d => ({x:d.a, y:d.c}))}
 
         />
       </VictoryChart>
