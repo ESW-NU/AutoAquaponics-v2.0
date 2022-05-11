1599b0b041eaf8eaac33dbf3b575b2fbe5953f71
LesterTai64 <92131170+LesterTai64@users.noreply.github.com>
2022-05-10 19:50:27 -0500

    Radio buttons and time entry progress

--- src/Components/OnOffTimer.js
+++ src/Components/OnOffTimer.js
@@ -1,1 +1,27 @@
 // on/off/timer buttons for lights and water pump pages
+
+import * as React from 'react';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormLabel from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const OnOffTimer = () => {
+    return (
+        <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Status</FormLabel>
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+            >
+                <FormControlLabel value="On" control={<Radio />} label="On" />
+                <FormControlLabel value="Off" control={<Radio />} label="Off" />
+                <FormControlLabel value="Timer" control={<Radio />} label="Timer" />
+            </RadioGroup>
+        </FormControl>
+    );
+}
+
+export default OnOffTimer
