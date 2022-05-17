36325500ef6b53d1ba046db6be73c58dd53e1f3a
Ben Caterine <benrcaterine@gmail.com>
2022-05-17 18:33:03 -0500

    control entry, flow entry components preliminary versions

--- src/Components/ControlEntry.js
+++ src/Components/ControlEntry.js
@@ -1,1 +1,13 @@
 // entry box and description for backwashing and oxygenator
+import TextField from '@mui/material/TextField';
+
+const ControlEntry = ({title}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <TextField id="entry" type="number" inputProps={{step:10}} />
+        </div>
+    );
+}
+
+export default ControlEntry;
--- src/Components/FlowEntry.js
+++ src/Components/FlowEntry.js
@@ -1,1 +1,16 @@
 // flow rate and pump time entries for a grow bed (water pump page)
+import { autocompleteClasses } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+const FlowEntry = () => {
+    return (
+        <div>
+            <p>Flow Rate (GPH)</p>
+            <TextField id="flowrate" type="number" inputProps={{step:10}} />
+            <p>Pump Time (min)</p>
+            <TextField id="pumptime" type="number" inputProps={{step:10}} />
+        </div>
+    );
+}
+
+export default FlowEntry;
