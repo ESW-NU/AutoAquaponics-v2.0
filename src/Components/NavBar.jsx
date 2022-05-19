99953d3919d7e938246003195b03aa8cfa138926
natbrew <nataliebrewster2023@u.northwestern.edu>
2022-05-19 01:08:45 -0500

    nav styled

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,67 +2,69 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import ResponsiveMenu from "react-responsive-navbar";
 import Grid from "@mui/material/Grid";
+// import Item from "@mui/material/Item";
 import "../CSS/NavBar.css";
 
 export const NavBar = () => {
   return (
-    // <header className="header">
-    //   <div className="content-container">
-    //     <div className="header__bar">
     <Grid
-  container
-  direction="row"
-  justifyContent="space-between"
-  alignItems="center"
->
-      <NavLink
-        to="/"
-        className={(isActive) =>
-          isActive ? "header__title is-active" : "header__title"
-        }
-      >
-        AUTOAQUAPONICS
-      </NavLink>
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Grid>
+        <NavLink
+          to="/"
+          className={(isActive) =>
+            isActive ? "header__title is-active" : "header__title"
+          }
+        >
+          AUTOAQUAPONICS
+        </NavLink>
+      </Grid>
 
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-      <NavLink
-        to="/Pages/VideoFeed"
-        className={(isActive) =>
-          isActive ? "header__nav is-active" : "header__nav"
-        }
-      >
-        Video Feed
-      </NavLink>
-      <NavLink
-        to="/Pages/Dashboard"
-        className={(isActive) =>
-          isActive ? "header__nav is-active" : "header__nav"
-        }
-      >
-        Dashboard
-      </NavLink>
-      <NavLink
-        to="/Pages/ControlPanel"
-        className={(isActive) =>
-          isActive ? "header__nav is-active" : "header__nav"
-        }
-      >
-        Control Panel
-      </NavLink>
-      <NavLink
-        to="/Pages/Settings"
-        className={(isActive) =>
-          isActive ? "header__nav is-active" : "header__nav"
-        }
-      >
-        Settings
-      </NavLink>
+      <Grid>
+        <NavLink
+          to="/Pages/VideoFeed"
+          className={(isActive) =>
+            isActive ? "header__nav is-active" : "header__nav"
+          }
+        >
+          Video Feed
+        </NavLink>
+        <NavLink
+          to="/Pages/Dashboard"
+          className={(isActive) =>
+            isActive ? "header__nav is-active" : "header__nav"
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/Pages/ControlPanel"
+          className={(isActive) =>
+            isActive ? "header__nav is-active" : "header__nav"
+          }
+        >
+          Control Panel
+        </NavLink>
+        <NavLink
+          to="/Pages/Settings"
+          className={(isActive) =>
+            isActive ? "header__nav is-active" : "header__nav"
+          }
+        >
+          Settings
+        </NavLink>
+      </Grid>
+
+      <Grid>
+        <button className="button-18" role="button">
+          LOGIN
+        </button>
+      </Grid>
     </Grid>
-    <button>testing</button>
-    </Grid>
-    //     </div>
-    //   </div>
-    // </header>
   );
 };
 export default NavBar;
