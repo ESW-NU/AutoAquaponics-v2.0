792c05f434703470e5c8eff3e7f5ca6aea0873b5
LesterTai64 <92131170+LesterTai64@users.noreply.github.com>
2022-05-29 04:10:44 -0500

    improved TimeEntry component

--- src/Components/TimeEntry.jsx
+++ src/Components/TimeEntry.jsx
@@ -1,78 +1,58 @@
 // start time picker component and duration dropdown/entries for lights page
 
+// select field imports
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import NativeSelect from '@mui/material/NativeSelect';
-import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  'label + &': {
-    marginTop: theme.spacing(3),
-  },
-  '& .MuiInputBase-input': {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-}));
+// textbox imports
+// import Box from '@mui/material/Box';
+import FilledInput from '@mui/material/FilledInput';
+// import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Input from '@mui/material/Input';
+// import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
-export default function CustomizedSelects() {
-  const [startHH, startMM, durHH, setdurHH, durMM, setdurMM] = React.useState('');
+export default function TimeEntry() {
+  const [startHH, setStartHH] = React.useState('');
+  const [startMM, setStartMM] = React.useState('');
+  const [meridiem, setMeridiem] = React.useState('');
+  const [durHH, setDurHH] = React.useState('');
+  const [durMM, setDurMM] = React.useState('');
+
   const handleChangeStartHH = (event) => {
-    setdurHH(event.target.value);
+    setStartHH(event.target.value);
   };
   const handleChangeStartMM = (event) => {
-    setdurHH(event.target.value);
+    setStartMM(event.target.value);
   };
-  const handleChange = (event) => {
-    setdurHH(event.target.value);
+  const handleChangeMeridiem = (event) => {
+    setMeridiem(event.target.value);
   };
-  const handleChange2 = (event) => {
-    setdurMM(event.target.value);
-  }
+  const handleChangeDurHH = (event) => {
+    setDurHH(event.target.value);
+  };
+  const handleChangeDurMM = (event) => {
+    setDurMM(event.target.value);
+  };
+
   return (
     <div>
       <div>
-        Time Picker (HH:MM)
-        <FormControl sx={{ m: 1 }} variant="standard">
-          <InputLabel id="demo-customized-select-label">HH</InputLabel>
+        Start Time (HH:MM)
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-filled-label">HH</InputLabel>
           <Select
-            labelId="demo-customized-select-label"
-            id="demo-customized-select"
-            value={durHH}
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={startHH}
             onChange={handleChangeStartHH}
-            input={<BootstrapInput />}
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={0}>0</MenuItem>
             <MenuItem value={1}>1</MenuItem>
             <MenuItem value={2}>2</MenuItem>
             <MenuItem value={3}>3</MenuItem>
@@ -85,110 +65,57 @@ export default function CustomizedSelects() {
             <MenuItem value={10}>10</MenuItem>
             <MenuItem value={11}>11</MenuItem>
             <MenuItem value={12}>12</MenuItem>
-            <MenuItem value={13}>13</MenuItem>
-            <MenuItem value={14}>14</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={16}>16</MenuItem>
-            <MenuItem value={17}>17</MenuItem>
-            <MenuItem value={18}>18</MenuItem>
-            <MenuItem value={19}>19</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={21}>21</MenuItem>
-            <MenuItem value={22}>22</MenuItem>
-            <MenuItem value={23}>23</MenuItem>
-
           </Select>
         </FormControl>
-        <FormControl sx={{ m: 1 }} variant="standard">
-          <InputLabel id="demo-customized-select-label">MM</InputLabel>
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-filled-label">MM</InputLabel>
           <Select
-            labelId="demo-customized-select-label"
-            id="demo-customized-select"
-            value={durMM}
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={startMM}
             onChange={handleChangeStartMM}
-            input={<BootstrapInput />}
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
             <MenuItem value={0}>0</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={25}>25</MenuItem>
-            <MenuItem value={30}>30</MenuItem>
-            <MenuItem value={35}>35</MenuItem>
-            <MenuItem value={40}>40</MenuItem>
-            <MenuItem value={45}>45</MenuItem>
-            <MenuItem value={50}>20</MenuItem>
-            <MenuItem value={55}>55</MenuItem>
-
-          </Select>
-        </FormControl>
-      </div>
-      <div>
-        Duration (HH and MM)
-        <FormControl sx={{ m: 1 }} variant="standard">
-          <InputLabel htmlFor="demo-customized-textbox">Duration (HH)</InputLabel>
-          <BootstrapInput id="demo-customized-textbox" />
-        </FormControl>
-        <FormControl sx={{ m: 1 }} variant="standard">
-          <InputLabel id="demo-customized-select-label">Duration (HH)</InputLabel>
-          <Select
-            labelId="demo-customized-select-label"
-            id="demo-customized-select"
-            value={durHH}
-            onChange={handleChange}
-            input={<BootstrapInput />}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={6}>6</MenuItem>
-            <MenuItem value={7}>7</MenuItem>
-            <MenuItem value={8}>8</MenuItem>
-            <MenuItem value={9}>9</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={11}>11</MenuItem>
-            <MenuItem value={12}>12</MenuItem>
-            <MenuItem value={13}>13</MenuItem>
-            <MenuItem value={14}>14</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={16}>16</MenuItem>
-            <MenuItem value={17}>17</MenuItem>
-            <MenuItem value={18}>18</MenuItem>
-            <MenuItem value={19}>19</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={21}>21</MenuItem>
-            <MenuItem value={22}>22</MenuItem>
-            <MenuItem value={23}>23</MenuItem>
-
-          </Select>
-        </FormControl>
-        <FormControl sx={{ m: 1 }} variant="standard">
-          <InputLabel id="demo-customized-select-label">Duration (MM)</InputLabel>
-          <Select
-            labelId="demo-customized-select-label"
-            id="demo-customized-select"
-            value={durMM}
-            onChange={handleChange2}
-            input={<BootstrapInput />}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
             <MenuItem value={10}>10</MenuItem>
             <MenuItem value={20}>20</MenuItem>
             <MenuItem value={30}>30</MenuItem>
             <MenuItem value={40}>40</MenuItem>
             <MenuItem value={50}>50</MenuItem>
-
+          </Select>
+        </FormControl>
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-filled-label">AM/PM</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={meridiem}
+            onChange={handleChangeMeridiem}
+          >
+            <MenuItem value="AM">AM</MenuItem>
+            <MenuItem value="PM">PM</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        Duration (HH and MM)
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120}}>
+          <InputLabel htmlFor="component-filled">Hours</InputLabel>
+          <FilledInput id="component-filled" value={durHH} onChange={handleChangeDurHH} />
+        </FormControl>
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-filled-label">Minutes</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={durMM}
+            onChange={handleChangeDurMM}
+          >
+            <MenuItem value={0}>0</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value={40}>40</MenuItem>
+            <MenuItem value={50}>50</MenuItem>
           </Select>
         </FormControl>
       </div>
