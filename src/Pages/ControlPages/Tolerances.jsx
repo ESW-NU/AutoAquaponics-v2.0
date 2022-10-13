aa1c9582696bd23b92e1a86fcf5c06183ac41a03
LesterTai64 <92131170+LesterTai64@users.noreply.github.com>
2022-10-12 19:46:19 -0500

    completed tolerances page

--- src/Pages/ControlPages/Tolerances.jsx
+++ src/Pages/ControlPages/Tolerances.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import ControlCard from "../../Components/ControlCard";
+import MaxMin from "../../Components/MaxMin";
 
 export const Tolerances = () => {
   return (
-    <Typography variant="body" align="left" padding="10px">
-      TOLERANCES CONTROL PANEL
-    </Typography>
+    <div>
+      <Typography variant="body" align="left" padding="10px">
+        TOLERANCES CONTROL PANEL
+      </Typography>
+      <Grid
+        container
+        spacing={1}
+        columns={{ xs: 1, sm: 1, md: 1, lg: 2, xl: 3 }}
+        >
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="pH" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="TDS (ppm)" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="Relative Humidity (%)" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="Air Temperature (ºC)" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="Water Temperature (ºC)" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+            <ControlCard title="Water Level (cm)" list={[
+              <MaxMin/>
+            ]}/>
+          </Grid>
+      </Grid>
+    </div>
   )
 }
 
