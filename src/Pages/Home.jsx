20ff61398d52374d6b92337882962c1bd11ee051
Andre Chen <andrexchen@gmail.com>
2022-10-25 18:32:16 -0500

    create branch

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,17 +16,22 @@ export const Home = () => {
         direction='row-reverse'
         spacing={5}
         justifyContent='center'
+        alignItems='center'
       >
-        <Grid item lg={3}>
-          <Typography variant="h1">NORTHWESTERN ESW AUTOAQUAPONICS</Typography>
-          <Typography variant="h3">
-            A fully automated aquaponic system in that can grow both fish and
-            plants unattended for one month and be monitored & controlled
-            remotely
-          </Typography>
+        <Grid item lg={2} className="borderwoo">
+            <Typography variant="h1">NORTHWESTERN ESW AUTOAQUAPONICS</Typography>
+            <Typography variant="h3">
+              A fully automated aquaponic system in that can grow both fish and
+              plants unattended for one month and be
+              <NavLink to="/dashboard">monitored</NavLink> &
+              <NavLink to="/control-panel">controlled</NavLink>
+              remotely
+            </Typography>
         </Grid>
-        <Grid item lg={3}>
-          <img src={systemCAD} />
+        <Grid item lg={3} className="borderwoo">
+          <div className="borderwoo">
+            <img src={systemCAD} className="objectFill"/>
+          </div>
         </Grid>
       </Grid>
     </div>
