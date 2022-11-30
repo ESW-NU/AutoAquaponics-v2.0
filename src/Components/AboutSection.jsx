08b7ee53fac5ce3249f389124f8cf39e85a14798
Ben Caterine <benrcaterine@gmail.com>
2022-11-29 18:46:37 -0600

    updated margins and multiple images

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -7,57 +7,134 @@ import "../CSS/pages.css"
 
 const systemCAD = require("../Lib/PlumbingCADclear.png");
 
-const AboutSection = ({ image_left, title, text }) => {
-    if (image_left) {
-        return (
-            <div className="App">
-                <div className="pages">
-                    <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
-                        <CardContent>
-                            <Grid
-                                container
-                                columns={5}
-                                alignItems='center'
-                            >
-                                <Grid item xs={2} size='contain'>
-                                    <img src={systemCAD} style={{width: '100%', height: '100%'}}/>
+const AboutSection = ({ image_left, title, text, images }) => {
+    console.log(images.length)
+    if (images.length > 1) {
+        if (image_left) {
+            return (
+                <div className="App">
+                    <div className="pages">
+                        <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
+                            <CardContent>
+                                <Grid
+                                    container
+                                    columns={5}
+                                    alignItems='center'
+                                >
+                                    <Grid item xs={2} size='contain'>
+                                        <Grid
+                                            container
+                                            columns={2}
+                                            alignItems='center'
+                                        >
+                                            <Grid item xs={1} size='contain'>
+                                                <img src={images[0]} style={{width: '100%', height: '100%'}}/>
+                                            </Grid>
+                                            <Grid item xs={1} size='contain'>
+                                                <img src={images[1]} style={{width: '100%', height: '100%'}}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography variant="h2">{title}</Typography>
+                                        <br></br>
+                                        <Typography variant="h6">{text}</Typography>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="h2">{title}</Typography>
-                                    <br></br>
-                                    <Typography variant="h6">{text}</Typography>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="App">
+                    <div className="pages">
+                        <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
+                            <CardContent>
+                                <Grid
+                                    container
+                                    columns={5}
+                                    alignItems='center'
+                                >
+                                    <Grid item xs={3}>
+                                        <Typography variant="h2">{title}</Typography>
+                                        <br></br>
+                                        <Typography variant="h6">{text}</Typography>
+                                    </Grid>
+                                    <Grid item xs={2} size='contain'>
+                                        <Grid
+                                            container
+                                            columns={2}
+                                            alignItems='center'
+                                        >
+                                            <Grid item xs={1} size='contain'>
+                                                <img src={images[0]} style={{width: '100%', height: '100%'}}/>
+                                            </Grid>
+                                            <Grid item xs={1} size='contain'>
+                                                <img src={images[1]} style={{width: '100%', height: '100%'}}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            )
+        }
     } else {
-        return (
-            <div className="App">
-                <div className="pages">
-                    <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
-                        <CardContent>
-                            <Grid
-                                container
-                                columns={5}
-                                alignItems='center'
-                            >
-                                <Grid item xs={3}>
-                                    <Typography variant="h2">{title}</Typography>
-                                    <br></br>
-                                    <Typography variant="h6">{text}</Typography>
+        if (image_left) {
+            return (
+                <div className="App">
+                    <div className="pages">
+                        <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
+                            <CardContent>
+                                <Grid
+                                    container
+                                    columns={5}
+                                    alignItems='center'
+                                >
+                                    <Grid item xs={2} size='contain'>
+                                        <img src={images[0]} style={{width: '100%', height: '100%'}}/>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography variant="h2">{title}</Typography>
+                                        <br></br>
+                                        <Typography variant="h6">{text}</Typography>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={2} size='contain'>
-                                    <img src={systemCAD} style={{width: '100%', height: '100%'}}/>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="App">
+                    <div className="pages">
+                        <Card sx={{ maxWidth: 2000, minHeight: 300 }}>
+                            <CardContent>
+                                <Grid
+                                    container
+                                    columns={5}
+                                    alignItems='center'
+                                >
+                                    <Grid item xs={3}>
+                                        <Typography variant="h2">{title}</Typography>
+                                        <br></br>
+                                        <Typography variant="h6">{text}</Typography>
+                                    </Grid>
+                                    <Grid item xs={2} size='contain'>
+                                        <img src={images[0]} style={{width: '100%', height: '100%'}}/>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            )
+        }
     }
 };
 
