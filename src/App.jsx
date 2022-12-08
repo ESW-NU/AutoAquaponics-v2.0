a0322790fb033286e454a8f5f3bef698e1493342
billyen33 <billmembers33@gmail.com>
2022-12-07 22:59:31 -0600

    test update website with pushes

--- src/App.jsx
+++ src/App.jsx
@@ -15,57 +15,6 @@ import { Lights } from "./Pages/ControlPages/Lights"
 import { Tolerances } from "./Pages/ControlPages/Tolerances";
 import { WaterPump } from "./Pages/ControlPages/WaterPump";
 
-//Set up Google Analytics Inside Project
-//App.js 
-/*
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = "UA-251456195-1"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-
-//React Router to track pages visited the most
-import React, {useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-ReactGA.initialize(TRACKING_ID);
-
-const ContactUs = () => (
-  <div>
-    <h3>Contact Us</h3>
-  </div>
-);
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const App = () => {
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contactus" element={<ContactUs />} />
-      </Routes>
-    </Router>
-  );
-}*/
-
 function App() {
   return (
     <>
