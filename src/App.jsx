d190425e582bbb327ed3a865cb42a0b0075792c0
Talia <taliabennaim@gmail.com>
2023-01-17 18:14:29 -0600

    added basic google analytics functionality using ga4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./Lib/styling";
 import React from "react";
+import {useEffect} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
@@ -14,8 +15,18 @@ import { FishFeeder } from "./Pages/ControlPages/FishFeeder";
 import { Lights } from "./Pages/ControlPages/Lights"
 import { Tolerances } from "./Pages/ControlPages/Tolerances";
 import { WaterPump } from "./Pages/ControlPages/WaterPump";
+import ReactGA from 'react-ga4';
+// const TRACKING_ID = "330623545"; // OUR_TRACKING_ID
+// const TRACKING_ID = "349563764"; // OUR_TRACKING_ID
+const TRACKING_ID = "G-XQDHE464FW";
+ReactGA.initialize(TRACKING_ID);
+
 
 function App() {
+	useEffect(() => {
+		ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+	// ReactGA.pageview(window.location.pathname + window.location.search);
+		}, []);
   return (
     <>
       <BrowserRouter>
