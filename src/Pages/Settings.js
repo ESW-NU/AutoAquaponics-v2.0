8cbf6a76759a214e826d9185ca2f347b641604b9
zrazzaq2026 <zeeshanrazzaq2026@u.northwestern.edu>
2023-01-31 18:51:53 -0600

    creates-basic-settingspage-#41

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -3,6 +3,28 @@ import ComingSoon from "../Components/ComingSoon";
 
 export const Settings = () => {
     return (
-        <ComingSoon/>
+        <div className="Pages">
+            <Grid
+        container
+        spacing={-2}
+        columns={{ xs: 1, sm: 1, md: 1, lg: 2, xl: 3 }}
+        >
+            
+  <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel htmlFor="component-filled">Phone Number</InputLabel>
+                <FilledInput type="number" id="component-filled" onChange={0}     />
+            </FormControl>
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 320 }}>
+                <InputLabel htmlFor="component-filled">Email</InputLabel>
+                <FilledInput type="string" id="component-filled" onChange={0} />
+            </FormControl>
+            <button className="button-18" role="button">
+          Save
+        </button>
+        </Grid>
+          
+        </div>
     );
 }
+
+// export default Settings;
