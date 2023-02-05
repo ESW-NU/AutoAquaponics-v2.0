dba45322235c1eb242e8c435460b62e5329c16c1
LesterTai64 <92131170+LesterTai64@users.noreply.github.com>
2023-02-05 14:24:11 -0600

    current progress for firebase logging

--- src/Components/MaxMin.jsx
+++ src/Components/MaxMin.jsx
@@ -2,28 +2,62 @@ import * as React from 'react';
 import FilledInput from '@mui/material/FilledInput';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { db } from '../firebase';
+import { doc, updateDoc } from "firebase/firestore";
 
-export default function MaxMin() {
-    const [min, setMin] = React.useState('');
-    const [max, setMax] = React.useState('');
+const MaxMin = ({sensor}) => { // notice that a string 'sensor' is our input
+    const [min, setMin] = React.useState(0);
+    const [max, setMax] = React.useState(0);
+    let minInput = React.createRef();
 
     const handleChangeMin = (event) => {
+        // console.log(sensor[0].props.sensor);
+        // console.log(sensor);
+        console.log(minInput);
         setMin(event.target.value);
+        // console.log(event.target.value);
+        // send to firebase
+        const minRef = doc(db, "tolerances", sensor);
+        updateDoc(minRef, {
+            min: +minInput
+        });
+
     };
     const handleChangeMax = (event) => {
+        console.log(sensor);
+        console.log(event);
         setMax(event.target.value);
+        console.log(max + "here");
+        // send to firebase
+        const maxRef = doc(db, "tolerances", sensor);
+        updateDoc(maxRef, {
+            max: +max
+        });
+
     };
+
+    // const handleSubmit = (event) => {
+    //     const minRef = doc(db, "tolerances", sensor);
+    //     updateDoc(minRef, {
+    //         min: min
+    //     })
+    // }
+
+    // handle firebase storing above. no save button would be here in this case
 
     return (
         <div>
             <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
                 <InputLabel htmlFor="component-filled">Min</InputLabel>
-                <FilledInput type="number" id="component-filled" value={min} onChange={handleChangeMin} />
+                <FilledInput ref={minInput} type="number" id="component-filled" value={min} onChange={(e) => handleChangeMin(e)} />
             </FormControl>
             <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
                 <InputLabel htmlFor="component-filled">Max</InputLabel>
-                <FilledInput type="number" id="component-filled" value={max} onChange={handleChangeMax} />
+                <FilledInput type="number" id="component-filled" value={max} onChange={(e) => handleChangeMax(e)} />
             </FormControl>
         </div>
     );
 }
+
+export default MaxMin;
--- src/Pages/ControlPages/Tolerances.jsx
+++ src/Pages/ControlPages/Tolerances.jsx
@@ -3,8 +3,37 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import ControlCard from "../../Components/ControlCard";
 import MaxMin from "../../Components/MaxMin";
+//import MakeChanges from "../../Components/MakeChanges";
+//import { collection, addDoc } from "firebase/firestore";
+//import db from "../../firebase";
 
 export const Tolerances = () => {
+  
+  // Push Function to be used by Save button "onClick"
+  // const Push = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const docRef = await addDoc(collection(db, "tolerances"), {
+  //       pH_min: pH_min,
+  //       pH_max: pH_max,
+  //       TDS_min: TDS_min,
+  //       TDS_max: TDS_max,
+  //       humidity_min: humidity_min,
+  //       humidity_max: humidity_max,
+  //       air_temp_min: air_temp_min,
+  //       air_temp_max: air_temp_max,
+  //       water_temp_min: water_temp_min,
+  //       water_temp_max: water_temp_max,
+  //       water_level_min: water_level_min,
+  //       water_level_max: water_level_max,
+  //     });
+  //     console.log("Document written with ID: ", docRef.id);
+  //   } catch (e) {
+  //     console.error("Error adding document: ", e);
+  //   }
+  // }
+
   return (
     <div>
       <Typography variant="body" align="left" padding="10px">
