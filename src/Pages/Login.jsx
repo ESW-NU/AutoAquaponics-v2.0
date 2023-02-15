22df988671f3b6fa1a507419cba1594a9010ca55
Talia <taliabennaim@gmail.com>
2023-02-14 19:05:41 -0600

    added logout functionality and debugging tools

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 function loginUser(){
 	const auth = getAuth();
@@ -7,8 +7,10 @@ function loginUser(){
 	let password = document.getElementsByName("password")[0].value;
 	signInWithEmailAndPassword(auth, email, password)
 	  .then((userCredential) => {
-		// Signed in 
-		const user = userCredential.user;
+		  console.log("SIGNED IN")
+		  // Signed in 
+		  const user = userCredential.user;
+		  console.log(user)
 	  })
 	  .catch((error) => {
 		const errorCode = error.code;
@@ -16,6 +18,33 @@ function loginUser(){
 	  });
 };
 
+function logoutUser(){
+	const auth = getAuth();
+		signOut(auth).then(() => {
+			console.log("SIGNED OUT")
+		// Sign-out successful.
+		}).catch((error) => {
+		// An error happened.
+		});
+};
+
+function getUser(){
+	const auth = getAuth();
+	onAuthStateChanged(auth, (user) => {
+	  if (user) {
+		console.log("CURRENT USER exists with uid: ")
+		// User is signed in, see docs for a list of available properties
+		// https://firebase.google.com/docs/reference/js/firebase.User
+		const uid = user.uid;
+		console.log(uid)
+		// ...
+	  } else {
+		console.log("NO USER LOGGED IN")
+		// User is signed out
+		// ...
+	  }
+	});
+}
 export const Login = () => {
 	return (
 	  <div className="App">
@@ -25,7 +54,9 @@ export const Login = () => {
 					<input type="email" id="email" name="email"/><br/>
 					<label for="password">Password:</label>
 					<input type="password" id="password" name="password"/><br/>
-					<button onClick={loginUser}>Submit</button>
+					<button onClick={loginUser}>Submit</button><br/>
+					<button onClick={logoutUser}>Logout</button><br/>
+					<button onClick={getUser}>Check user</button>
 				</div>
 			</div>
 		</div>
