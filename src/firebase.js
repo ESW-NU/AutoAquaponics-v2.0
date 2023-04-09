5b4998f22b29edbe96bbc2a6526576c85ba212d8
andrechen77 <andrexchen@gmail.com>
2023-04-09 14:07:21 -0500

    add app check

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-import {
-    initializeApp
-} from 'firebase/app'
-import {
-    getFirestore
-} from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
     apiKey: "AIzaSyASROLzF7oKnE5klKSznqJnBbFzt5B0Cpw",
@@ -16,4 +13,15 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+
+// The following line will generate an App Check debug token that should be registered in the Firebase
+// in order to verify API calls. This bypasses the ReCaptcha attestation, but requires access to the
+// console. Production builds should have the following line set to false.
+self.FIREBASE_APPCHECK_DEBUG_TOKEN = true; // eslint-disable-line no-restricted-globals
+const reCaptchaPublicKey = '6LcriVwlAAAAADRhpcZMGLPXslMx4QH2KTfTn5tt';
+const appCheck = initializeAppCheck(app, { // eslint-disable-line no-unused-vars
+    provider: new ReCaptchaV3Provider(reCaptchaPublicKey),
+    isTokenAutoRefreshEnabled: true,
+});
+
+export const db = getFirestore(app);
