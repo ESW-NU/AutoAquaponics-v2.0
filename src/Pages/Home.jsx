f6c5957b2e278ae4decc50d397fcbe8ccbd2a642
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2023-04-09 14:16:46 -0500

    Revert ""added animation for the title on the homepage""

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,6 @@
 import { Box, Stack, Typography, Link, Grid } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import AboutSection from "../Components/AboutSection";
-import Typed from 'react-typed'
 
 const systemCAD = require("../Lib/PlumbingCADclear.webp");
 const plumbing = require("../Lib/plumbing.webp");
@@ -33,7 +32,7 @@ const Home = () => {
 			<Stack direction={{ xs: "column-reverse", md: "row" }} alignItems="center">
 				<img style={{ width: "50vw" }} src={systemCAD} alt="System CAD"/>
 				<Box>
-					<Typography variant="h1"><Typed strings={["Northwestern AutoAquaponics"]} typeSpeed={40} onComplete={(self) => self.cursor.remove()}/></Typography>
+					<Typography variant="h1">Northwestern AutoAquaponics</Typography>
 					<Typography variant="body1">
 						A fully automated aquaponic system that grows both fish and plants
 						unattended for months and can be <Link component={RouterLink} to="/dashboard">monitored</Link> and <Link component={RouterLink} to="/control-panel/tolerances">controlled</Link> remotely
