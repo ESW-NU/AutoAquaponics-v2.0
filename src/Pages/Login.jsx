f1eab2471ee2fd08fa43f9861e204092d8ceecd9
Talia <taliabennaim@gmail.com>
2023-04-14 15:40:03 -0500

    refactored login form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,46 +1,55 @@
 import React from "react";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "firebase/auth";
-
-function loginUser(){
-	const auth = getAuth();
-	let email = document.getElementsByName("email")[0].value;
-	let password = document.getElementsByName("password")[0].value;
-	signInWithEmailAndPassword(auth, email, password)
-	  .then((userCredential) => {
-		// Signed in 
-		console.log("SIGNED IN")
-		const user = userCredential.user;
-		console.log(user)
-	  })
-	  .catch((error) => {
-		const errorCode = error.code;
-		const errorMessage = error.message;
-	  });
-};
-
-function logoutUser(){
-	const auth = getAuth();
-		signOut(auth).then(() => {
-			console.log("SIGNED OUT")
-		// Sign-out successful.
-		}).catch((error) => {
-		// An error happened.
-		});
-};
-
+import { getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import { useState } from "react";
 
 const Login = () => {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const auth = getAuth();
+
+	const handleLogout = () => {
+		const auth = getAuth();
+			signOut(auth).then(() => {
+				console.log("SIGNED OUT")
+			// Sign-out successful.
+			}).catch((error) => {
+			// An error happened.
+				console.error(error);
+			});
+	};
+	
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
+			console.log("SIGNED IN")
+			// Sign-in successful.
+		} catch (error) {
+			// An error happened.
+			console.error(error);
+		}
+	};
+
 	return (
 	  <div className="App">
 			<div className="Pages">
-				<div>
-					<label for="email">Email:</label>
-					<input type="email" id="email" name="email"/><br/>
-					<label for="password">Password:</label>
-					<input type="password" id="password" name="password"/><br/>
-					<button onClick={loginUser}>Submit</button>
-					<button onClick={logoutUser}>Log Out</button>
-				</div>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="Email"
+					/>
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+					/>
+					<button type="submit">Log In</button>
+					<button onClick={handleLogout}>Log Out</button>
+				</form>
 			</div>
 		</div>
 	);
