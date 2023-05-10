51a533d27049597edaa2a6aa674b26e32c5796c1
miyaliu627 <107294785+miyaliu627@users.noreply.github.com>
2023-05-09 19:27:45 -0500

    changed the drop down nav bard

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,10 +2,18 @@
 
 import { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import { Grid, Stack, Box, useMediaQuery, Typography, Button, IconButton, Collapse } from '@mui/material';
+import { Grid, Stack, Box, useMediaQuery, Typography, Button, IconButton, Collapse, Paper } from '@mui/material';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import MenuOpenRoundedIcon from '@mui/icons-material/MenuOpenRounded';
 import theme from "../styling";
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import BuildIcon from '@mui/icons-material/Build';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const links = [
 	{ addr: "/video-stream", label: "Video Stream" },
@@ -42,9 +50,46 @@ export const NavBar = () => {
 					</Grid>
 				</Grid>
 			</Stack>
-			<Collapse in={isSmall && open}>
-				<NavBarLinks links={links} />
-			</Collapse>
+			
+      <Collapse in={open} timeout="auto" unmountOnExit>
+		<Paper elevation={3} sx={{mx:3, marginBottom:5}}>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 3 }} href="/video-stream" label={"Video Stream"} divider={true}> 
+		  	<ListItemIcon>
+              	<VideocamIcon />
+            </ListItemIcon>
+		  	<ListItemText><Typography variant={"body2"}>Video Stream</Typography></ListItemText>
+          </ListItemButton>
+
+		  <ListItemButton sx={{ pl: 3 }} href="/dashboard" label={"Dashboard"} divider={true}> 
+		  	<ListItemIcon>
+              	<DashboardIcon />
+            </ListItemIcon>
+			<ListItemText><Typography variant={"body2"}>Dashboard</Typography></ListItemText>
+          </ListItemButton>
+
+		  <ListItemButton sx={{ pl: 3 }} href="/control-panel" label={"Control Panel"} divider={true}> 
+		  	<ListItemIcon>
+              	<BuildIcon />
+            </ListItemIcon>
+			<ListItemText><Typography variant={"body2"}>Control Panel</Typography></ListItemText>
+          </ListItemButton>
+
+		  <ListItemButton sx={{ pl: 3 }} href="settings" label={"setting"} divider={true}> 
+		  	<ListItemIcon>
+              	<SettingsIcon />
+            </ListItemIcon>
+			<ListItemText><Typography variant={"body2"}>Settings</Typography></ListItemText>
+          </ListItemButton>
+        </List>
+		</Paper>
+      </Collapse>
+
+			
+			{/* <Collapse in={isSmall && open}>
+				<NavBarLinks links={links}/>
+				
+			</Collapse> */}
 		</Box>
 	);
 };
