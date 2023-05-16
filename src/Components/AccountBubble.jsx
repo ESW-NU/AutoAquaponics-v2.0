e2934ce1c5352c37f33ca4aa0b37a891b35722c7
Talia <taliabennaim@gmail.com>
2023-05-16 18:31:14 -0500

    using react Menu component

--- src/Components/AccountBubble.jsx
+++ src/Components/AccountBubble.jsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
-import { Avatar } from '@mui/material';
+import React from "react";
+import { Button, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { UserContext } from "../Hooks/UserContext";
@@ -9,43 +9,57 @@ import theme from "../styling";
 
 const AccountBubble = () => {
     
+  const user = useContext(UserContext);
+  const letter = user.email[0].toUpperCase();
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   const handleLogout = () => {
-    signOut(auth).then(() => {
-      setShowLogoutBox(false);
+    signOut(auth).then(() => { 
+      setAnchorEl(null);
       navigate("/");
     }).catch((error) => {
       console.error(error);
     });
   };
   
-  const [showLogoutBox, setShowLogoutBox] = useState(false);
-  const user = useContext(UserContext);
-  const letter = user.email[0].toUpperCase();
-  const navigate = useNavigate();
-  
   return(
     <div style={{ position: 'relative' }}>
-      <Avatar 
-        onClick={() => setShowLogoutBox(!showLogoutBox)} 
-        sx={{bgcolor: theme.palette.primary.dark}}
-      >
-        {letter}
-      </Avatar>
-      {showLogoutBox && (
-      <Button 
-        variant="contained" 
-        color="clickme"
-        style={{
-          position: 'absolute',
-          top: '110%',
-          left: '-100%',
-          zIndex: 999
+      <IconButton
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}>
+        <Avatar 
+          sx={{bgcolor: theme.palette.primary.dark}}
+        >
+          {letter}
+        </Avatar>
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
         }}
-        onClick={handleLogout}
       >
-        Logout
-      </Button>
-      )}
+        <MenuItem  
+          onClick={handleLogout}
+        >
+          Logout
+        </MenuItem>
+      </Menu>
     </div>	
   )
 };
