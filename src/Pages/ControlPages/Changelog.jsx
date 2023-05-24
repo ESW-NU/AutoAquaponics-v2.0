f5e60e1cabfe9f3903a99f67682d451c1c1a04f2
zaech727 <zacharymccoy2026@u.northwestern.edu>
2023-05-23 19:11:18 -0500

    changelog table

--- src/Pages/ControlPages/Changelog.jsx
+++ src/Pages/ControlPages/Changelog.jsx
@@ -1,37 +1,36 @@
 import { forwardRef } from "react";
-import { Paper, Stack, Typography, Grid } from "@mui/material";
+import { Paper, Stack, Typography, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 const data = [
 	{ a: "one", b: "two", c: "three" },
 	{ a: "uno", b: "dos", c: "tres" },
 ];
 
-/* This code is copypasted lol take a look at https://mui.com/material-ui/react-table/#virtualized-table
-I still haven't copypasted everything over yet; trying to make sure I understand what's going on - Andre */
-/* const VirtuosoTableComponents = {
-	Scroller: forwardRef((props, ref) => (
-		<TableContainer component={Paper} {...props} ref={ref} />
-	)),
-	Table: (props) => (
-		<Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
-	),
-	TableHead,
-	TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
-	TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
-} */
-
 const Changelog = () => {
 	return (
 		<Stack spacing={1}>
 			<Typography variant="h2">Changelog</Typography>
-			<Paper sx={{ p: 2 }}>
-				Table goes here
-				{/* <TableVirtuoso
-					data={data}
-					components={VirtuosoTableComponents}
-					fixedHeaderContent={fixedHeaderContent}
-					itemContent={rowContent}
-				/> */}
+			<Paper sx={{ p: 2, height: '65vh', overflow: 'auto' }}>
+				<TableContainer>
+					<Table stickyHeader>
+						<TableHead>
+							<TableRow>
+								<TableCell style={{width: '20%'}}>Name</TableCell>
+								<TableCell style={{width: '20%'}}>Date</TableCell>
+								<TableCell>Changes</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{data.map((item, index) => (
+								<TableRow key={index}>
+									<TableCell>{item.a}</TableCell>
+									<TableCell>{item.b}</TableCell>
+									<TableCell>{item.c}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
 			</Paper>
 		</Stack>
 	);
