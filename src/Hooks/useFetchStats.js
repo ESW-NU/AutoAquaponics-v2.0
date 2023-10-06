20be138372f925746b9fd89119131b088ea9dbdb
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-10-06 14:41:56 -0500

    onSnapshot realtime listener to replace getDocs one time fetching in useFetchStats

--- src/Hooks/useFetchStats.js
+++ src/Hooks/useFetchStats.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { collection, getDocs, query, orderBy, where } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, where, onSnapshot } from "firebase/firestore";
 import { db } from '../firebase';
 import { systemStatMeta } from "../systemMeta";
 
@@ -29,27 +29,33 @@ export function useFetchStats(timeBounds) {
 	const [stats, setStats] = useState([]);
 	useEffect(() => {
 		setLoading(true);
-		getDocs(
-			query(
-				collection(db, 'stats'),
-				where('unix_time', '>', timeBounds[0]),
-				where('unix_time', '<', timeBounds[1]),
-				orderBy('unix_time', 'asc')
-			)
-		).then(snapshot => {
-			setStats(convertStatsSnapshot(snapshot));
+    const q = query( // the data we want to fetch
+      collection(db, 'stats'),
+      where('unix_time', '>', timeBounds[0]),
+      where('unix_time', '<', timeBounds[1]),
+      orderBy('unix_time', 'asc')
+    );
+    const unsubscribe = onSnapshot(q, (snapshot) => { // real-time listener for changes to the queried stats data
+      setStats(convertStatsSnapshot(snapshot));
 			setLoading(false);
-		});
+    });
+    return () => {
+      unsubscribe(); // make sure to unsubscribe when the component unmounts cause idek but like efficiency probably
+    }
 	}, [timeBounds[0], timeBounds[1]]); // specify the bounds individually, otherwise React thinks the bounds changed and will re-run the Effect
 
 	// get tolerances
 	const [tolerances, setTolerances] = useState({});
 	useEffect(() => {
-		getDocs(
-			query(
-				collection(db, 'tolerances'),
-			)
-		).then(snapshot => setTolerances(convertTolerancesSnapshot(snapshot)));
+    const q = query(
+      collection(db, 'tolerances')
+    )
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setTolerances(convertTolerancesSnapshot(snapshot))
+    });
+    return() => {
+      unsubscribe()
+    }
 	}, []);
 
 	return { loading, stats, tolerances };
