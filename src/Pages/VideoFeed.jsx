cb12e72ecdde71920ba82aeffdaebc34ab536e57
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2024-05-05 12:55:34 -0500

    add latency measurements

--- src/Pages/VideoFeed.jsx
+++ src/Pages/VideoFeed.jsx
@@ -6,6 +6,7 @@ export const VideoFeed = () => {
     const videoRef = useRef();
     const streamHostnameInputRef = useRef();
     const [activeStreamHostname, setActiveStreamHostname] = useState("127.0.0.1:8080");
+    const [measuredLatency, setMeasuredLatency] = useState(0);
 
     const hlsSupported = Hls.isSupported();
 
@@ -15,20 +16,26 @@ export const VideoFeed = () => {
         } else {
             const hls = new Hls({
                 debug: false,
-                liveSyncDurationCount: 2, // try to be about 2 segments away from the newest segment
+                liveSyncDurationCount: 1, // try to be about 1 segment away from the newest segment
                 liveMaxLatencyDurationCount: 5, // don't be more than 5 segments away from newest
                 maxLiveSyncPlaybackRate: 1.5, // allow speedup of up to 1.5x to catch up to live
             });
             hls.loadSource(`http://${activeStreamHostname}/stream.m3u8`);
             hls.attachMedia(videoRef.current);
 
+            // initialize latency measurement
+            const latencyMeasurementIntervalId = setInterval(() => {
+                setMeasuredLatency(hls.latency);
+            }, 100);
+
             // autoplay
-            videoRef.current.play();
+            videoRef.current.play().catch(() => { /* do nothing */ });
 
             // TODO error handling
 
             return () => {
                 hls.destroy();
+                clearInterval(latencyMeasurementIntervalId);
             };
         }
     }, [activeStreamHostname]);
@@ -52,6 +59,7 @@ export const VideoFeed = () => {
                 <Alert severity="error">HLS is not supported in your browser</Alert>
             )}
             <video ref={videoRef} width="100%" controls></video>
+            latency: {measuredLatency}
         </div>
     );
 }
